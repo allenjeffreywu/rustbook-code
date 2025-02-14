f230534d6fb3b2fc610f375b7da9df3d816bc4db
Allen Wu <allenwuhoo@gmail.com>
2025-02-13 23:28:24 -0800

    partial chapter 12 prepare for unit tests

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -3,9 +3,9 @@
 // to read commandline arguments, you might want to use crates.io in the future,
 // but since we're learning this we'll want to do it ourselves
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 // this file should only focus on running the program
 
@@ -30,7 +30,7 @@ fn main() {
 
     // reading a file
     // use if let here because we don't care about the returned value, (). better to only handle the error case with if let
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
@@ -43,36 +43,36 @@ fn main() {
 }
 
 // let's be explicitly clear with what the return is. Settling for a tuple is not good enough
-struct Config {
-    query: String,
-    file_path: String,
-}
+// struct Config {
+//     query: String,
+//     file_path: String,
+// }
 
-// creating a constructor for config
-impl Config {
-    // more complicated constructor that returns a Result. We now require explicit error handling
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+// // creating a constructor for config
+// impl Config {
+//     // more complicated constructor that returns a Result. We now require explicit error handling
+//     fn build(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
-    }
-}
+//         Ok(Config { query, file_path })
+//     }
+// }
 
 // Box dyn error
 // dyn here means dynamic
 // box here is a trait object
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+// fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+//     println!("With text:\n{contents}");
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 // fn parse_config(args: &[String]) -> Config {
 //     // the tradeoffs using clone
