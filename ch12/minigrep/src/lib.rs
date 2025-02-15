6df26cea04fc0d099477fabbba5d56253354d1ed
Allen Wu <allen.wu@utexas.edu>
2025-02-14 23:59:59 -0800

    ch12 tdd done

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -22,9 +22,51 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // https://doc.rust-lang.org/reference/expressions/operator-expr.html#the-question-mark-operator
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+// defining search just enough for the test to compile
+// lifetime variable 'a
+// telling rust that the data returned by search function will ive as long as the data passed into the search function in the contents argument
+// important because the data referenced by a slice needs to be valid for the reference to be valid. If the compiler assumes we are making string slices of query rather than contents, it will do its safety checking incorrectly.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+// Test Driven Development
+// 1. Write a test that fails and run to make sure it fails for the reason you expect
+// 2. Write or modify just enough code to make the new test pass
+// 3. Refactor the code you just added or changed and make sure the tests continue to pass
+// 4. Repeat from step 1
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        // the backslash after opening double quote tells Rust not to put a newline character at the beginning of the contents of this string literal
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
