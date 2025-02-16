cc1280e7798368a4b289612a959f93d061558a6c
Allen Wu <allen.wu@utexas.edu>
2025-02-15 23:59:59 -0800

    ch12 env var done

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -1,11 +1,13 @@
 // this file should focus on the logic behind the program
 
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -17,7 +19,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -25,7 +33,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // https://doc.rust-lang.org/reference/expressions/operator-expr.html#the-question-mark-operator
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -48,6 +62,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+// `IGNORE_CASE=1 cargo run -- to poem.txt``
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase(); // to_lowercase creates new data (String) rather than referencing existing data. Also would need more work to handle unicode.
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 // Test Driven Development
 // 1. Write a test that fails and run to make sure it fails for the reason you expect
 // 2. Write or modify just enough code to make the new test pass
@@ -59,14 +87,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        // the backslash after opening double quote tells Rust not to put a newline character at the beginning of the contents of this string literal
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
